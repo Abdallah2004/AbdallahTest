51790958a3172b5460045f6390b2e0bd9815d9f7
Abdallah2004 <akhayoussef@gmail.com>
2019-11-23 01:13:24 +0100

    new app

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,7 @@
+import os
+
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:Neptune2004_@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI=os.environ['postgres://hrliqysnjbjdgw:67b169670dd95bac7676f136bba51832682a8403073d86f76e815a4e1c858da7@ec2-54-228-243-238.eu-west-1.compute.amazonaws.com:5432/d72qs1qv3dphch']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
